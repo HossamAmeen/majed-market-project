0c42194a530935949eb7397f16f24f8560b71aaf
hossam <hossamameen948@gmail.com>
2020-09-22 21:22:04 +0200

    first commit

---
+++ app/Http/Controllers/BackEnd/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\BackEnd;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use Image , Auth;
+
+class UserController extends BackEndController
+{
+    public function __construct(User $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function store(Request $request){
+    //    return $request->all();
+
+        $requestArray = $request->all();
+        if(isset($requestArray['password']) )
+        $requestArray['password'] =  Hash::make($requestArray['password']);
+        // if(isset($requestArray['image']) )
+        // {
+        //     $fileName = $this->uploadImage($request );
+        //     $requestArray['image'] =  $fileName;
+        // }
+
+        $requestArray['user_id'] = Auth::user()->id;
+        $this->model->create($requestArray);
+        session()->flash('action', 'تم الاضافه بنجاح');
+       
+        return redirect()->route($this->getClassNameFromModel().'.index');
+    }
+
+    public function update($id , Request $request){
+
+
+
+        $row = $this->model->FindOrFail($id);
+        $requestArray = $request->all();
+        if(isset($requestArray['password']) && $requestArray['password'] != ""){
+            $requestArray['password'] =  Hash::make($requestArray['password']);
+        }else{
+            unset($requestArray['password']);
+        }
+        // if(isset($requestArray['image']) )
+        // {
+        //     $fileName = $this->uploadImage($request );
+        //     $requestArray['image'] =  $fileName;
+        // }
+
+        $requestArray['user_id'] = Auth::user()->id;
+        $row->update($requestArray);
+
+       
+        session()->flash('action', 'تم التحديث بنجاح');
+        return redirect()->route($this->getClassNameFromModel().'.index');
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+Route::prefix('admin')->group(function(){
+    Auth::routes();
+
+    Route::any('sendToken' , 'BackEnd\ConfigrationController@sendToken')->name('forget.password');
+    Route::any('paswordreset/{id}/{token}' , 'BackEnd\ConfigrationController@paswordreset');
+    // Route::post('login', 'BackEnd\UserController@login');
+    Route::middleware('auth')->namespace('BackEnd')->group(function () {
+
+        
+
+        Route::get('/', 'ConfigrationController@index');
+        Route::resource('configrations', 'ConfigrationController');
+        Route::resource('users', 'UserController');
+        
+    });
+});
+
